d28d78d87e5ed393c516a63fc312145b68f12f9c
Brian Zhu <brianzhu0512@gmail.com>
2019-10-14 18:07:54 -0400

    Created the jsTPS Transaction/jsTPS class file.

---
+++ js/jsTPS.js
@@ -0,0 +1,143 @@
+class jsTPS_Transaction
+{
+    doTransaction()
+    {
+
+    }
+
+    undoTransaction()
+    {
+
+    }
+}
+
+class jsTPS
+{
+    constructor()
+    {
+        this.transactions = [];
+        this.mostRecentTransaction = -1;
+        this.performingDo = false;
+        this.performingUndo = false;
+    }
+
+    isPerformingDo()
+    {
+        return this.performingDo;
+    }
+
+    isPerformingUndo()
+    {
+        return this.performingUndo;
+    }
+
+    getSize()
+    {
+        return this.transactions.length;
+    }
+
+    addTransaction(transaction)
+    {
+        if ((this.mostRecentTransaction < 0) || (this.mostRecentTransaction < (this.getSize() - 1)))
+        {
+            for (var i = this.getSize() - 1; i > this.mostRecentTransaction; i--)
+            {
+                this.transactions.splice(i, 1);
+            }
+        }
+
+        this.transactions.push(transaction);
+
+        this.doTransaction();
+    }
+
+    hasTransactionToRedo()
+    {
+        return this.mostRecentTransaction < (this.getSize() - 1);
+    }
+
+    hasTransactionToUndo()
+    {
+        return this.mostRecentTransaction >= 0;
+    }
+
+    doTransaction()
+    {
+        if (this.hasTransactionToRedo())
+        {
+            this.performingDo = true;
+            let transaction = this.transactions[this.mostRecentTransaction + 1];
+            transaction.doTransaction();
+            this.mostRecentTransaction++;
+            this.performingDo = false;
+        }
+    }
+
+    peekUndo()
+    {
+        if (this.hasTransactionToUndo())
+        {
+            return this.transactions[this.mostRecentTransaction];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    peekDo()
+    {
+        if (this.hasTransactionToRedo())
+        {
+            return this.transactions[this.mostRecentTransaction + 1];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    undoTransaction()
+    {
+        if (this.hasTransactionToUndo())
+        {
+            this.performingUndo = true;
+            let transaction = this.transactions[this.mostRecentTransaction];
+            transaction.undoTransaction();
+            this.mostRecentTransaction--;
+            this.performingUndo = false;
+        }
+    }
+
+    clearAllTransactions()
+    {
+        this.transactions.splice(0, this.getSize());
+        this.mostRecentTransaction = -1;
+    }
+
+    getRedoSize()
+    {
+        return this.getSize() - this.mostRecentTransaction - 1;
+    }
+
+    getUndoSize()
+    {
+        return this.mostRecentTransaction + 1;
+    }
+
+    toString()
+    {
+        let text = "--Number of Transactions: " + this.getSize() + "\n";
+        text += "--Current Index on Stack: " + this.mostRecentTransaction + "\n";
+        text += "--Current Transaction Stack:\n";
+        
+        for (var i = 0; i <= this.mostRecentTransaction; i++)
+        {
+            let jT = this.transactions[i];
+            text += "----" + jT.toString() + "\n";
+        }
+
+        return text;
+    }
+
+}
